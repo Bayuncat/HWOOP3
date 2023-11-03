9289aeb12c763a3828de1a78fac1f39fa72668bc
Igor Borisov <113010453+Bayuncat@users.noreply.github.com>
2023-11-03 14:08:29 +0300

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,48 @@
+package org.example;
+
+public class Student implements Comparable<Student>{
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    private Long studentId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String middleName;
+
+    @Override
+    public int compareTo(Student o) {
+        return this.studentId.compareTo(o.studentId);
+    }
+}
